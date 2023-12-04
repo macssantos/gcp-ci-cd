cc56ef658396ea2e69d84d1bdf286ce5205298cc
Geovane de Freitras Queiroz Morcatti <>
2023-12-04 16:44:01 -0300

    multa

---
+++ test_multa.py
@@ -0,0 +1,38 @@
+import pytest
+from biblioteca import Biblioteca
+from cliente import Cliente
+
+from multa import MultasPorAtraso
+
+@pytest.fixture
+def biblioteca():
+    return Biblioteca("Biblioteca Teste")
+
+@pytest.fixture
+def cliente():
+    return Cliente("João", 25, "Estudante")
+
+@pytest.fixture
+def livro():
+    return Livro("Aventuras na Biblioteca", "Autor Teste", "123456789")
+
+@pytest.fixture
+def multas():
+    return MultasPorAtraso(taxa_multa_por_dia=5.0)
+
+def test_aplicar_multa(biblioteca, cliente, livro, multas):
+    biblioteca.adicionar_cliente(cliente)
+    biblioteca.adicionar_livro(livro)
+    cliente.entrar_na_biblioteca(biblioteca)
+
+    biblioteca.emprestar_livro(livro.isbn)
+    livro.disponivel = False
+
+    # Simulando atraso de 3 dias
+    multas.aplicar_multa(cliente, livro, dias_atraso=3)
+
+    # Verifica se a multa foi aplicada corretamente
+    multas_cliente = multas.obter_multas_cliente(cliente)
+    assert livro.titulo in multas_cliente
+    assert multas_cliente[livro.titulo]['valor'] == 15.0
+    assert 'data_aplicacao' in multas_cliente[livro.titulo]
